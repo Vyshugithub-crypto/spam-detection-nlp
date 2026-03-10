f9fa7bbba9a4555ab4018be97832bb2bbc21493e
Vyshugithub-crypto <vaishnavikongalla7@gmail.com>
2026-03-10 19:09:49 +0530

    Update spam_checker.py

--- notebooks/spam_checker.py
+++ notebooks/spam_checker.py
@@ -1,21 +1,9 @@
 import pickle
-
-# load saved model
 model = pickle.load(open("spam_model.pkl", "rb"))
-
-# load vectorizer
 vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
-
-# take user input
 msg = input("Enter SMS message: ")
-
-# convert message to vector
 msg_vector = vectorizer.transform([msg])
-
-# predict
 prediction = model.predict(msg_vector)
-
-# show result
 if prediction[0] == 1:
     print("This message is SPAM")
 else:
