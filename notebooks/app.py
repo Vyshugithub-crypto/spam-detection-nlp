1d07b5b912e446581b38278e63cdb661f472802f
Vyshugithub-crypto <vaishnavikongalla7@gmail.com>
2026-03-10 19:08:00 +0530

    Update app.py

--- notebooks/app.py
+++ notebooks/app.py
@@ -1,21 +1,15 @@
 import streamlit as st
 import pickle
-
-# Load model and vectorizer
 model = pickle.load(open("../models/spam_model.pkl", "rb"))
 vectorizer = pickle.load(open("../models/vectorizer.pkl", "rb"))
 st.title("📩 SMS Spam Detector")
-
 st.write("Type a message and press Enter or click Check.")
-
 with st.form("spam_form"):
     message = st.text_input("Enter SMS message")
     submit = st.form_submit_button("Check Message")
-
 if submit and message:
     msg_vector = vectorizer.transform([message])
     prediction = model.predict(msg_vector)
-
     if prediction[0] == 1:
         st.error("🚫 This message is SPAM")
     else:
